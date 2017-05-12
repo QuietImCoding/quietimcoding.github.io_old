9fa80de4582885b469a32df97d6f7bf570002d2a
Daniel Monteagudo <dmonte3210@gmail.com>
2017-05-12 18:35:46 -0400

    lettuce see if this works

---
+++ password/pass.js
@@ -0,0 +1,26 @@
+var textbox, pass;
+
+var httpRequest = function(site) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+	    // Action to be performed when the document is read;
+	    pass.innerHTML = xhttp.responseText;
+	}
+    };
+    xhttp.open("GET", "http://danwashere.ddns.net/" + site);
+    xhttp.send();
+};
+
+window.onload = function() {
+    txtbox = document.getElementById("noot");
+    pass = document.getElementById("pass");
+    txtbox.oninput = function(e) {
+	if (txtbox.value != "") {
+	    httpRequest(txtbox.value);
+	} else {
+	    pass.innerHTML = "";
+	}
+    }
+    //alert("hello");
+}
