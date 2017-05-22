2887078d45f24769f76f29c3b0bed93eacab7a8e
Daniel Monteagudo <dmonte3210@gmail.com>
2017-05-22 14:50:52 -0400

    i forgot the devlog

--- password/pass.js
+++ password/pass.js
@@ -12,6 +12,23 @@ var httpRequest = function(site) {
     xhttp.send();
 };
 
+var updoot = function() {
+    var lorenz = document.getElementById('lorenz');
+    var name = document.getElementById('name');
+    var age = document.getElementById('age');
+    var pkey = document.getElementById('pkey');
+    var button = document.getElementById('button');
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+	    // Action to be performed when the document is read;
+	    pass.innerHTML = xhttp.responseText;
+	}
+    };
+    xhttp.open("GET", "https://qiller.net/api/passgen/" + site);
+    xhttp.send();
+}
+
 window.onload = function() {
     txtbox = document.getElementById("noot");
     pass = document.getElementById("pass");
