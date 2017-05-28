ecf86c12d89b8f3997952f674fc6f7eec570e237
Daniel Monteagudo <dmonte3210@gmail.com>
2017-05-28 03:48:28 -0400

    ah yes

--- password/pass.js
+++ password/pass.js
@@ -15,18 +15,21 @@ var httpRequest = function(site) {
 var updoot = function() {
     var lorenz = document.getElementById('lorenz');
     var name = document.getElementById('name');
+    var city = document.getElementById('city');
     var age = document.getElementById('age');
     var pkey = document.getElementById('pkey');
     var button = document.getElementById('button');
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
-	    // Action to be performed when the document is read;
-	    pass.innerHTML = xhttp.responseText;
+	    lorenz.parentNode.removeChild(lorenz);
+	    console.log(xhttp.responseText);
 	}
     };
-    xhttp.open("GET", "https://qiller.net/api/passgen/" + site);
-    xhttp.send();
+    var params = 'city='+city.value+'&name='+name.value+'&age='+age.value+'&pkey='+pkey;
+    xhttp.open("POST", "https://qiller.net/api/passgen/updoot");
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(params);
 }
 
 window.onload = function() {
